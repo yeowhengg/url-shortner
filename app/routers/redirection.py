29f35dd2e004268ebcbd2de4e0bfbc7d04dd4888
yeowhengg <yeowheng1999@gmail.com>
2025-01-06 02:53:14 +0800

    .env for domain:port

--- app/routers/redirection.py
+++ app/routers/redirection.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter, Response
 from fastapi.responses import RedirectResponse
 from ..database import db_session
 from ..helper import check_link
+import os
+from dotenv import load_dotenv
+load_dotenv()
+os.getenv
+DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 
 SessionDep = db_session.SessionDep
 
@@ -9,5 +14,5 @@ router = APIRouter()
 
 @router.get("/{shortcode}")
 def redirect_route(shortcode: str, session: SessionDep):
-    test = check_link.get_link("http://127.0.0.1:8000/"+shortcode, session)
+    test = check_link.get_link(f"{DOMAIN_NAME}"+shortcode, session)
     return RedirectResponse(url=test)
